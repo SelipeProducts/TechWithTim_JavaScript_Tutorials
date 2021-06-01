fc74a9fcc20df91ffca4adb3c09cb3dcd2034408
Cesar Felipe Lopez <>
2021-06-01 22:22:52 +0000

    Commented out different ways to write on console and on document.
    First Set Value to Document's ID Element. Then wrote Value to Document.
    
    Finally wrote line trying to Log Input Value inside text box

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-//Different ways to write 
-//1)console
-//2)document
-console.log("Hello! Is there anybody in there!")
+// //Different ways to write 
+// //1)console
+// //2)document
+// console.log("Hello! Is there anybody in there!")
 
-document.write("<h1> Hello!? </h1>")
+// document.write("<h1> Hello!? </h1>")
 
+// //--------------------------------------------------
+// //Alternative Console Writing
+// console.log("Hi this is just another log.")
+// console.warn("Warning! Warning! New Warning Unlocked!")
+// console.error("Error Error. Does not compute")
 
-//--------------------------------------------------
-//Alternative Console Writing
-console.log("Hi this is just another log.")
-console.warn("Warning! Warning! New Warning Unlocked!")
-console.error("Error Error. Does not compute")
+console.log(document.getElementById("header"))
+
+console.log(document.getElementById("header").innerHTML)
+
+//Setting Value to Document's ID Element 
+document.getElementById("header").innerHTML = "This is NOW THE NEW HEADER!"
+
+console.log(document.getElementById("header").innerHTML)
+
+//-----------------------------------------------------
+//Writing Value to Document
+document.write(document.getElementById("header").innerHTML)
+document.write(" <==That was not a header")
+document.write("<h3> But this is a header</h3>")
+
+//-----------------------------------------------------
+//Trying to Log Input Value inside text box
+//This line of code does not work by itself
+//Bc HTML and JS read from top to bottom
+//When page is ran value is logged but it is blank
+console.log(document.getElementById("myInput").value)
